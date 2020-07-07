9f64e2aa442938e6480f1e1cd20143e3ca6fcaba
Neil MacDougall <neil.macdougall@suse.com>
2020-07-07 15:51:15 +0100

    Always show

--- src/reporter.js
+++ src/reporter.js
@@ -20,19 +20,13 @@ module.exports = function ProtractorBrowserLogReporter() {
   this.flushLogs = function(result) {
     // Flush browser logs between tests. Ref: https://stackoverflow.com/questions/30582297/clear-console-log-in-browser-using-protractor
     return browser.driver.manage().logs().get('browser').then((logsEntries) => {
-      // Output logs to console on failure. Ref: http://eitanp461.blogspot.com/2014/01/advanced-protractor-features.html
-      if(result.status === 'failed') {
-        if (logsEntries.length === 0) {
-          console.log('\nNo browser logs were generated for test:', result.fullName);
-          console.log('');
-        } else {
-          console.log('\nBrowser logs for test:', result.fullName);
-          logsEntries.forEach((logEntry) => {
-            let timestamp = moment(logEntry.timestamp).format('h:mm:ss');
-            console.log(timestamp, logEntry.type, logEntry.message);
-          });
-          console.log('');
-        }
+      if (logsEntries.length > 0) {
+        console.log('\nBrowser logs for test:', result.fullName);
+        logsEntries.forEach((logEntry) => {
+          let timestamp = moment(logEntry.timestamp).format('h:mm:ss');
+          console.log(timestamp, logEntry.type, logEntry.message);
+        });
+        console.log('');
       }
     }).catch((err) => {
       if(result.status === 'failed') {
